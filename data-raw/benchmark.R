107bc1c913ab82ff0f043773be344fba26ad667a
schochastics <david@schochastics.net>
2025-01-28 11:25:43 +0100

    updated with new package

--- data-raw/benchmark.R
+++ data-raw/benchmark.R
@@ -1,13 +1,15 @@
-library(adaR)
+# pak::pak("adaR")
+# pak::pak("dyfanjones/urlparse")
 top100 <- readLines("https://raw.githubusercontent.com/ada-url/url-various-datasets/main/top100/top100.txt")
 
 bench::mark(check=FALSE,
-  ada_url_parse("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag"),
-  rs_url_parse("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag")
+  adaR::ada_url_parse("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag"),
+  urlparser::rs_url_parse("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag"),
+  urlparse::url_parse("https://user_1:password_1@example.org:8080/dir/../api?q=1#frag"),
 )
 bench::mark(check=FALSE,
-  ada_url_parse(top100),
-  rs_url_parse(top100)
+  adaR::ada_url_parse(top100),
+  urlparser::rs_url_parse(top100),
+  urlparse::url_parse_v2(top100)
 )
 
-urlparser::
