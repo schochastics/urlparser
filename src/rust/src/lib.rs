d0e58e70ec676fab1cbd95676b15d73cd8498b91
schochastics <david@schochastics.net>
2025-01-30 09:54:02 +0100

    simplified rust code

--- src/rust/src/lib.rs
+++ src/rust/src/lib.rs
@@ -5,52 +5,54 @@ use url::Url;
 #[extendr]
 #[derive(Debug, IntoDataFrameRow)]
 struct ParsedUrl {
-    url: String,
-    scheme: String,
-    host: String,
-    port: String,
-    path: String,
-    query: String,
-    fragment: String,
-    username: String,
-    password: String
+  url: String,
+  scheme: String,
+  host: String,
+  port: String,
+  path: String,
+  query: String,
+  fragment: String,
+  username: String,
+  password: String,
 }
 
 #[extendr]
 fn url_parse(urls: Vec<String>) -> Dataframe<ParsedUrl> {
-    urls.into_iter()
-        .map(|url| {
-            Url::parse(&url)
-                .map(|parsed_url| ParsedUrl {
-                    url: url.clone(),
-                    scheme: parsed_url.scheme().to_string(),
-                    host: parsed_url.host_str().unwrap_or("").to_string(),
-                    port: parsed_url.port().map_or("".to_string(), |p| p.to_string()),
-                    path: parsed_url.path().to_string(),
-                    query: parsed_url.query().unwrap_or("").to_string(),
-                    fragment: parsed_url.fragment().unwrap_or("").to_string(),
-                    username: parsed_url.username().to_string(),
-                    password: parsed_url.password().unwrap_or("").to_string(),
-                })
-                .unwrap_or_else(|_| ParsedUrl {
-                    url: url.clone(),
-                    scheme: "".to_string(),
-                    host: "".to_string(),
-                    port: "".to_string(),
-                    path: "".to_string(),
-                    query: "".to_string(),
-                    fragment: "".to_string(),
-                    username: "".to_string(),
-                    password: "".to_string(),
-                })
+  urls.into_iter()
+    .map(|url| {
+      let parsed_url = Url::parse(&url);
+      let (scheme, host, port, path, query, fragment, username, password) =
+        parsed_url.as_ref().ok().map_or_else(
+          || Default::default(),
+          |p| (
+            p.scheme().to_string(),
+            p.host_str().unwrap_or("").to_string(),
+            p.port().map_or_else(String::new, |p| p.to_string()),
+            p.path().to_string(),
+            p.query().unwrap_or("").to_string(),
+            p.fragment().unwrap_or("").to_string(),
+            p.username().to_string(),
+            p.password().unwrap_or("").to_string(),
+          ),
+        );
+
+        ParsedUrl {
+            url,
+            scheme,
+            host,
+            port,
+            path,
+            query,
+            fragment,
+            username,
+            password,
+        }
         })
         .collect::<Vec<_>>() 
-        .into_dataframe()
+        .into_dataframe() 
         .unwrap()
 }
-// Macro to generate exports.
-// This ensures exported functions are registered with R.
-// See corresponding C code in `entrypoint.c`.
+
 extendr_module! {
     mod urlparser;
     fn url_parse;
